f776ea022627ea66fa7002e4cea60cda28618f38
daniildr <45598499+daniildr@users.noreply.github.com>
2020-10-13 00:56:49 +0300

    Added map on mode

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import router from "./router";
 import VModal from 'vue-js-modal'
+import { store } from './store'
 
 Vue.use(VModal)
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        test: "test-1-2-3"
+    },
+    getters : {},
+    mutations: {},
+    actions : {}
+})
